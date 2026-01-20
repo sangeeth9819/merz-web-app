fc3a7d2b0fa5528d9d74e5652b8b9536418cefb5
Sangeeth Gamage <sangeeth.shehan.zincat@gmail.com>
2026-01-20 22:02:34 +0530

    feat: add Google Sans Flex font to entire app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,21 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-// Using Inter as a temporary font until Google Sans is downloaded
-// To use Google Sans Flex: download from https://fonts.google.com/specimen/Google+Sans+Flex
-// and place font files in /public/fonts/
-const inter = Inter({
-  subsets: ["latin"],
+// Using Google Sans Flex font
+const googleSansFlex = localFont({
+  src: [
+    { path: "../public/fonts/GoogleSansFlex_24pt-Thin.ttf", weight: "100", style: "normal" },
+    { path: "../public/fonts/GoogleSansFlex_24pt-ExtraLight.ttf", weight: "200", style: "normal" },
+    { path: "../public/fonts/GoogleSansFlex_24pt-Light.ttf", weight: "300", style: "normal" },
+    { path: "../public/fonts/GoogleSansFlex_24pt-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../public/fonts/GoogleSansFlex_24pt-Medium.ttf", weight: "500", style: "normal" },
+    { path: "../public/fonts/GoogleSansFlex_24pt-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "../public/fonts/GoogleSansFlex_24pt-Bold.ttf", weight: "700", style: "normal" },
+    { path: "../public/fonts/GoogleSansFlex_24pt-ExtraBold.ttf", weight: "800", style: "normal" },
+    { path: "../public/fonts/GoogleSansFlex_24pt-Black.ttf", weight: "900", style: "normal" },
+  ],
   variable: "--font-google-sans",
   display: "swap",
 });
@@ -26,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${inter.variable} font-sans antialiased`}
+        className={`${googleSansFlex.variable} font-sans antialiased`}
         suppressHydrationWarning
       >
         <ThemeProvider
